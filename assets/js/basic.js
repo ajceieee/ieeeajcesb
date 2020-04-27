52e6ca0693f6f0feeb30b4e80cb20f26d476cd86
shan-shaji <shanshaji224@gmail.com>
2020-04-27 23:57:22 +0530

    Added basic.js

---
+++ assets/js/basic.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    const date = new Date();
+    const year = date.getFullYear();
+  $(".copyright").text(
+    `©  ${year} IEEE SB AJCE. Maintained  by Shan Shaji`
+  );
+
+ 
+  
+});
