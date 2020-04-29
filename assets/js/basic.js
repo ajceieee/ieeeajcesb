2ba427537f1d4271e14e82da69790daa8cb47e70
shan-shaji <shanshaji224@gmail.com>
2020-04-29 15:20:53 +0530

    Execom members updated

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
-    const date = new Date();
-    const year = date.getFullYear();
+  const date = new Date();
+
+  const Developer = {
+    name: "Shan Shaji",
+    year: date.getFullYear(),
+  };
+
   $(".copyright").text(
-    `©  ${year} IEEE SB AJCE. Maintained  by Shan Shaji`
+    `©  ${Developer.year} IEEE SB AJCE. Maintained  by ${Developer.name}`
   );
 
- 
   
 });
