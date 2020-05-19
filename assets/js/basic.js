50e38d77203fd89b93d104c84b5e38c3d88a5da5
shan-shaji <shanshaji224@gmail.com>
2020-05-19 15:15:23 +0530

    Execom Card Color changed and Updated News

--- assets/js/basic.js
+++ assets/js/basic.js
@@ -10,5 +10,117 @@ $(document).ready(function () {
     `©  ${Developer.year} IEEE SB AJCE. Maintained  by ${Developer.name}`
   );
 
-  
+  // News Updates
+  let badge = 0;
+  const UpdateNews = (newsUpdates) => {
+    if (newsUpdates) {
+      const newsList = {
+        news: [
+          {
+            title: "Webinar",
+            description:
+              "Developing a Culture of Continual Learning & Personal Branding",
+            date: 19,
+            month: "May",
+            year: 2020,
+            time: "5:00 pm",
+          },
+          {
+            title: "Webinar",
+            description:
+              "Open Source Programming & Design of Custom Library for Arduino IDE",
+            date: 20,
+            month: "May",
+            year: 2020,
+            time: "7:30 pm",
+          },
+          {
+            title: "Webinar",
+            description: "Invisibility Cloak",
+            date: 21,
+            month: "May",
+            year: 2020,
+            time: "4:00 pm",
+          },
+          {
+            title: "Webinar",
+            description: "Introduction To Machine Learning",
+            date: 22,
+            month: "May",
+            year: 2020,
+            time: "6:00 pm",
+          },
+          {
+            title: "Webinar",
+            description: "Volunteering with IEEE",
+            date: 23,
+            month: "May",
+            year: 2020,
+            time: "6:00 pm",
+          },
+          {
+            title: "Webinar",
+            description: "Canva",
+            date: 24,
+            month: "May",
+            year: 2020,
+            time: "6:00 pm",
+          },
+        ],
+      };
+
+      // To get Month Name
+      const monthNames = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+      ];
+
+      let todaysDate = new Date();
+      let date = todaysDate.getDate();
+      let month = monthNames[todaysDate.getMonth()];
+      let year = todaysDate.getFullYear();
+
+      // number of news
+      badge = 0;
+      // Updates the element with text new Updates
+      $("#news-updates").text("New Updates");
+      // Empties the Ul Element
+      $(".news-list").empty();
+
+      // Based on the number of news the cards are generated
+      newsList.news.forEach((news) => {
+        // checks whether the date of the event is greater than the present date
+        if (date <= news.date && news.month == month && news.year == year) {
+          // increment the value based on the nuuumber of news.
+          badge++;
+          $(".badge-notify").text(`${badge}`);
+          // will append the card for each news.
+          $(".news-list").append(
+            `<div class="card-news-list"><div class="card-title"><strong>${news.title}</strong></div><div class="card-content">${news.description}</div><div class="card-footer">${news.date}<sup>th</sup>${news.month} ${news.year} - ${news.time}</div></div>`
+          );
+        }
+      });
+    }
+  };
+
+  // If there is new update this as true else as false
+  const newsUpdates = true;
+
+  // Checks for if there is any newsupdates
+  if (newsUpdates) {
+    UpdateNews(newsUpdates);
+  } else {
+    $("#news-updates").text("No News Updates");
+    $(".badge-notify").text(`${badge}`);
+  }
 });
