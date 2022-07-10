7d8cbe9edb52f4ddd7f63936b425957581446e2c
Nebin Mathew John <nebinnmj@gmail.com>
2022-07-10 14:56:14 +0530

    SSCS event CIRCUITHON event added

--- Societies/SSCS/sscs-events.js
+++ Societies/SSCS/sscs-events.js
@@ -2,13 +2,21 @@ $(document).ready(function () {
 	let events2022 = [
 		{
 			eventName: "CIRCUITHON",
-			image: "../../events/images/2022/CIRCUITHON.png",
+			image: "../../events/images/2022/CIRCUITHON.jpg",
 			eventDate: "26th June 2022",
 			// venue: "Online",
 			// time: "3:00pm - 4:00pm",                            
 			// participants: 27,
 			report:
-				"<strong>CIRCUITHON</strong> is a national online circuit design competition.<br/>Date of submission: 26 June 2022<br/>More Details will be published soon.",
+				"You never fail until you stop trying ~    Albert Einstein <br/>" +
+				"Hey everyone! 🎊<br/>" +
+				"Greetings from IEEE SB AJCE! Circuits are never easy to understand. But once you master it, there is no going back. If you are one such master, here is a design competition for you!" +
+				"The SSCS chapter of IEEE SB AJCE is delighted to announce CIRCUITHON, a national level online competition on circuit design!<br/>"  +
+				"Prizes worth 4K 💰" +
+				"Last date for submission - 26th June 2022.<br/>" +
+				"<a href='https://bit.ly/Rules_and_Regulations_CIRCUITHON_SSCS_AJCE'>👉Rules & Regulations</a><br/>" +
+				"What are you waiting for? Go out there and electrify the circuits ⚡️ " +
+				"<a href='https://bit.ly/sscs_ajce_CIRCUITHON'>👉Submission Link</a><br/>",
 		}
 	];
 	let events = [...events2022];
@@ -17,7 +25,7 @@ $(document).ready(function () {
 			.append(`<div class="card border-dark mb-3 rounded event-card" >
      <div class="row no-gutters">
        <div class="col-md-4">
-       <a class="venobox" href="${event.image}"><img src="${event.image}" alt="image alt" class="card-img card-img-class "/></a>
+       <a class="venobox" href="${event.image}"><img src="${event.image}" style="height: 100%;width: 100%;" alt="image alt" class="card-img card-img-class "/></a>
        </div>
        <div class="col-md-8">
          <div class="card-body">
